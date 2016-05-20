02ef86fbc04bd84e4a63a057ae32fd48086b6265
Andrey Lesnikov <ozkriff@gmail.com>
2016-05-20 12:14:34 +0300

    vs.glsl: position: vec2 -> vec3

--- vs.glsl
+++ vs.glsl
@@ -1,12 +1,12 @@
 uniform lowp mat4 matrix;
 
-attribute lowp vec2 position;
+attribute lowp vec3 position;
 attribute lowp vec2 tex_coords;
 
 varying lowp vec2 v_tex_coords;
 
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+    gl_Position = vec4(position, 1.0) * matrix;
     v_tex_coords = tex_coords;
 }
 
