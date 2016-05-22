4219e758cd1f76ce6a960c9c01c51ddd32c95798
Andrey Lesnikov <ozkriff@gmail.com>
2016-05-22 09:48:46 +0300

    vs.glsl: Added model matrix

--- vs.glsl
+++ vs.glsl
@@ -1,4 +1,5 @@
 uniform mat4 view_mat;
+uniform mat4 model_mat;
 
 attribute vec3 position;
 attribute vec2 tex_coords;
@@ -6,7 +7,7 @@ attribute vec2 tex_coords;
 varying vec2 v_tex_coords;
 
 void main() {
-    gl_Position = view_mat * vec4(position, 1.0);
+    gl_Position = view_mat * model_mat * vec4(position, 1.0);
     v_tex_coords = tex_coords;
 }
 
