83bc92fa4922ca3f7c5d3e464bb156069af256ae
Andrey Lesnikov <ozkriff@gmail.com>
2016-05-28 01:09:14 +0300

    WIP: fs.glsl: Исправил название юниформа текстуры
    Иначе только последняя загруженная текстура работает

--- fs.glsl
+++ fs.glsl
@@ -1,9 +1,9 @@
-uniform sampler2D texture;
+uniform sampler2D tex;
 
 varying vec2 v_uv;
 
 void main() {
-    gl_FragColor = texture2D(texture, v_uv);
+    gl_FragColor = texture2D(tex, v_uv);
 }
 
 // vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
